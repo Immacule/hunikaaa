e65860b222911746f7d10fc15a17a7b8508e08e4
Brad traversy <me@bradtraversy.com>
2022-01-31 23:18:53 +0200

    changes

--- ussd/views.py
+++ ussd/views.py
@@ -44,7 +44,10 @@ def ussdApp(request):
             product=""
             response = "CON shyiramo nimero y irangamuntu "+str(product)+"\n"
         elif category =='1*1' and int(len(level)) == 3 and str(level[2]) in  str(level):
-            response = "CON shyiramo ibiro \n"
+            response = "CON ubwoko bw ibihingwa \n"
+            response += "1. ibinyameke \n"
+            response += "2. ibinyamafufu \n"
+            response += "3. imboga n imbuto"
         elif category =='1*1' and int(len(level)) == 4 and str(level[3]) in  str(level):
             response = "CON Shyiramo igihe bizahunikwa \n"
             response += "1. ukwezi \n"
@@ -60,7 +63,7 @@ def ussdApp(request):
             idnumber=idnumber,
             )
             insert.save()
-            response = "END Murakoze gukoresha hunikapp \n"
+            response = "END Murakoze kwiyandikisha kuri hunikapp\n"
 
 
         elif text == '1*2':
